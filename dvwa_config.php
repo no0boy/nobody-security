067d325fce14547f03e0fe9490f407d420dfa328
no0boy <no0boy@github.com>
2026-06-13 02:48:56 +0800

    修复时间线 [object Object] 渲染 bug

---
+++ dvwa_config.php
@@ -0,0 +1,9 @@
+<?php
+$DBMS = 'MySQL';
+$_DVWA = array();
+$_DVWA['db_server'] = 'localhost';
+$_DVWA['db_database'] = 'dvwa';
+$_DVWA['db_user'] = 'dvwa';
+$_DVWA['db_password'] = 'p@ssw0rd';
+$_DVWA['recaptcha_public_key'] = '';
+$_DVWA['recaptcha_private_key'] = '';
